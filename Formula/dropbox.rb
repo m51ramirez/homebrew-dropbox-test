ad39588f1283cd94ba7fd620254796f74e472e82
mdube99 <mjdube99@gmail.com>
2026-04-27 12:20:51 -0500

    init

--- Formula/dropbox.rb
+++ Formula/dropbox.rb
@@ -2,7 +2,7 @@ class Dropbox < Formula
     desc "dropbox-test"
     homepage "https://github.com/m51ramirez/"
     url "https://github.com/m51ramirez/dropbox-test/releases/download/v1.34.5/dropbox-1.34.5.tar.gz"
-    sha256 "aa3f71c8d812521d79381d3ed093d7a3f9e1e5ec2014c691df82832d95eb670f"
+    sha256 "951eeb711933ab659c905cd5742975c0cfe805fe825c3389fc9360f3ad849441"
     version "1.34.5"
 
     def install

6ab8c2c4465a925d5a3e1f6ee7ae4743924d5d00
mdube99 <mjdube99@gmail.com>
2026-04-27 11:53:34 -0500

    Update dropbox formula and add v1.34.5 release tarball

--- Formula/dropbox.rb
+++ Formula/dropbox.rb
@@ -2,7 +2,7 @@ class Dropbox < Formula
     desc "dropbox-test"
     homepage "https://github.com/m51ramirez/"
     url "https://github.com/m51ramirez/dropbox-test/releases/download/v1.34.5/dropbox-1.34.5.tar.gz"
-    sha256 "ec17a0d666376de9f925808daa3f2e8aff9aea62c047b8ccf4cc0afa0cf675b7"
+    sha256 "fa1ffdb53c82436fb8f3cd3a62c77d10fc3c41ad5194ba307ed49bc5f45b5954"
     version "1.34.5"
 
     def install
